3f2302fbc74ac05e83d2e0aac4f4fbb7d0a5124a
divyagautam001 <110798916+divyagautam001@users.noreply.github.com>
2024-03-04 09:28:05 +0530

    Update SyntaxEx.java

--- Google/SyntaxEx.java
+++ Google/SyntaxEx.java
@@ -37,3 +37,28 @@ public int fn(ListNode head) {
 
     return ans;
 }
+
+-------------------------------------------------------
+
+// Graph
+
+public int fn(int[][] graph) {
+    Stack<Integer> stack = new Stack<>();
+    Set<Integer> seen = new HashSet<>();
+    stack.push(START_NODE);
+    seen.add(START_NODE);
+    int ans = 0;
+
+    while (!stack.empty()) {
+        int node = stack.pop();
+        // do some logic
+        for (int neighbor: graph[node]) {
+            if (!seen.contains(neighbor)) {
+                seen.add(neighbor);
+                stack.push(neighbor); // recursion: ans += dfs(neighbor, graph);
+            }
+        }
+    }
+
+    return ans;
+}
